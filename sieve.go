702c8527f42bab7175bde77a60e8600ed7568b78
rtv3 <art@AWOLart.com>
2013-10-14 19:17:17 -0400

    Modified .gitignore for ide artifacts and maven. Updated sieve.go for new bit routine and for mongodb call.

--- sieve.go
+++ sieve.go
@@ -2,6 +2,9 @@
 
 package main
 
+import "encoding/binary"
+import "fmt"
+
 // Send the sequence 2, 3, 4, ... to channel 'ch'.
 func Generate(ch chan<- int) {
 	for i := 2; ; i++ {
@@ -20,16 +23,25 @@ func Filter(in <-chan int, out chan<- int, prime int) {
 	}
 }
 
+type Packet struct {
+	buffer []byte
+}
+
+func (p *Packet) Int16(i int) int16 {
+	return int16(binary.BigEndian.Uint16(p.buffer[i : i+2]))
+}
 // The prime sieve: Daisy-chain Filter processes.
 func main() {
 	ch := make(chan int) // Create a new channel.
 	go Generate(ch)      // Launch Generate goroutine.
-	for i := 0; i < 100; i++ {
+	for i := 0; i < 9; i++ {
 		prime := <-ch
 		print(prime, "\n")
 		ch1 := make(chan int)
 		go Filter(ch, ch1, prime)
 		ch = ch1
 	}
+	p := &Packet{buffer: []byte{0x01, 0x02, 0x04}}
+	fmt.Println("\nBytes: ", p.Int16(1))
 }
 
